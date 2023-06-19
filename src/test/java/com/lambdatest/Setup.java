5036fd2db644636ecb7afc912f73f270cdc4cb43
MohammaeKamle <mohammed.kamle@yahoo.com>
2023-06-19 13:48:39 +0530

    changed way of uploading and injecting image

--- src/test/java/com/lambdatest/Setup.java
+++ src/test/java/com/lambdatest/Setup.java
@@ -2,32 +2,43 @@ package com.lambdatest;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import okhttp3.*;
+import org.json.JSONObject;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Setup {
     public AndroidDriver<AndroidElement> driver;
+    protected String mediaUrl;
     @BeforeMethod
-    public void beforeMethod() throws MalformedURLException {
+    public void beforeMethod() throws IOException {
+
+        mediaUrl = uploadImageAndGetUrl();
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("build", "Camera injection feature build");
-        capabilities.setCapability("name", "Android_Camera_injection_test");
+        capabilities.setCapability("build", "debug");
+        //capabilities.setCapability("name", "Android_Camera_injection_test");
         capabilities.setCapability("deviceName", "Pixel.*");
-        capabilities.setCapability("platformVersion", "11");
+        //capabilities.setCapability("platformVersion", "11");
         capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("autoAcceptAlert", true);
+        capabilities.setCapability("autoGrantPermission", true);
         capabilities.setCapability("isRealMobile", true);
         //AppURL (Create from Wikipedia.apk sample in project)
-        capabilities.setCapability("app", "YOUR_APP_URL"); //Enter your app url
+        capabilities.setCapability("app", "camera-inject"); //Enter your app url
         capabilities.setCapability("deviceOrientation", "PORTRAIT");
         capabilities.setCapability("console", true);
         capabilities.setCapability("network", true);
         capabilities.setCapability("visual", true);
         capabilities.setCapability("devicelog", true);
         capabilities.setCapability("enableImageInjection",true);
-        capabilities.setCapability("media","YOUR_MEDIA_URL"); // eg: lt://MEDIAd508b730c7f84895aac014d584de0a07
+       // capabilities.setCapability("media","lt://MEDIA9186b4f4d60f459eb93555e0f9330bc3"); // eg: lt://MEDIAd508b730c7f84895aac014d584de0a07
 
         String username = System.getenv("LT_USERNAME");
         String accessKey = System.getenv("LT_ACCESS_KEY");
@@ -38,5 +49,29 @@ public class Setup {
     @AfterMethod(alwaysRun=true)
     public void tearDown() throws Exception {
         driver.quit();
+    }
+
+    private String uploadImageAndGetUrl() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+
+        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("media_file","IMG_2020.jpg",
+                        RequestBody.create(MediaType.parse("application/octet-stream"),
+                                new File("/Users/mohammadk/Downloads/IMG_2024.PNG")))
+                .addFormDataPart("type","image")
+                .addFormDataPart("custom_id","my-photo")
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://mobile-mgm.lambdatest.com/mfs/v1.0/media/upload")
+                .method("POST", body)
+                .addHeader("Authorization", "Basic bW9oYW1tYWRrOnJha2NCb0JZSGl5OEJXN29zVmk0TjFMR1lqZ0poUmZBd3ZMMXBQVXZ3Q0Exd2ZOQ2hk")
+                .build();
+        Response response = client.newCall(request).execute();
+        String jsonString = response.body().string();
+        JSONObject jo = new JSONObject(jsonString);
+        return (String) jo.get("media_url");
     }
 }
--- src/test/java/com/lambdatest/Test.java
+++ src/test/java/com/lambdatest/Test.java
@@ -2,13 +2,18 @@ package com.lambdatest;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidElement;
+import okhttp3.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Test extends Setup{
     @org.testng.annotations.Test
-    public void cameraInjectionTest() throws InterruptedException {
+    public void cameraInjectionTest() throws InterruptedException, IOException {
         WebDriverWait wait = new WebDriverWait(driver, 30);
 
         MobileElement element1 = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.android.permissioncontroller:id/permission_allow_button")));
@@ -17,10 +22,18 @@ public class Test extends Setup{
         MobileElement element2 = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.bsstag.cameraimage:id/button")));
         element2.click();
 
+
+        driver.executeScript("lambda-image-injection="+mediaUrl);
+
+
+
         MobileElement element4 = (AndroidElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.google.android.GoogleCamera:id/shutter_button")));
         element4.click();
 
         MobileElement element5 = (AndroidElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("com.google.android.GoogleCamera:id/shutter_button")));
         element5.click();
     }
+
+
+
 }
